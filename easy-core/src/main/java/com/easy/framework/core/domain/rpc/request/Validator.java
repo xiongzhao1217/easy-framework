80dff4395901a8a1e76000689fcd9fa9b1aebe99
xiongzhao1217 <xiongzhao1217@163.com>
2021-07-07 14:53:50 +0800

    Create Validator.java

---
+++ easy-core/src/main/java/com/easy/framework/core/domain/rpc/request/Validator.java
@@ -0,0 +1,16 @@
+package com.easy.framework.core.domain.rpc.request;
+
+/**
+ * RPC接口入参父类，包含内控合规参数
+ *
+ * @author xiongzhao
+ * @date 2021/06/15
+ */
+public interface Validator {
+
+    /**
+     * 需要子类实现的自定义校验
+     * 校验不通过需要抛{@link InvalidParamException}异常
+     */
+    void validate() throws InvalidParamException;
+}
